81646cb074c9e1556a675979537102c159ac3d3b
Alex Gian <AlexHGian@gmail.com>
2017-10-14 16:01:43 -0700

    prod build

--- sw.js
+++ sw.js
@@ -20,11 +20,11 @@ importScripts('workbox-sw.prod.v2.1.0.js');
 const fileManifest = [
   {
     "url": "build/agc.js",
-    "revision": "3f6a5197e9a14936675014c36ae30d94"
+    "revision": "777be0159e93a6510f3b463d2608e5a5"
   },
   {
     "url": "build/agc.registry.json",
-    "revision": "0a1361091420bb4f30c658acd0ab2b15"
+    "revision": "dc3730c93312c8f5754bacc86d0cd913"
   },
   {
     "url": "build/agc/agc.ihqj46bf.js",
@@ -35,16 +35,32 @@ const fileManifest = [
     "revision": "ffd2fd2b9f318fd596508f92d5e2fd25"
   },
   {
-    "url": "build/agc/ajksi7gf.css",
-    "revision": "6aacbf0ff4c6282e8a2b79a0b27e5c92"
+    "url": "build/agc/esdty7xs.js",
+    "revision": "256ab4b6e164ec874d659ce55407a886"
   },
   {
-    "url": "build/agc/oi8xe9hd.js",
-    "revision": "e51ead3b27c50ff6ea91645f5dae6ca0"
+    "url": "build/agc/k5fzxems.js",
+    "revision": "4c8fcfe998721396e6d52effab776de4"
+  },
+  {
+    "url": "build/agc/l9c6jken.css",
+    "revision": "b0f1292f9f7ebbb94b016022850a952c"
+  },
+  {
+    "url": "build/agc/nwdm6iqe.js",
+    "revision": "0f8992f93f283163d6ba04aac1a302d2"
+  },
+  {
+    "url": "build/agc/nz8dsqvs.css",
+    "revision": "71fc4d2bc0c61eb26a99f412535f8c0e"
+  },
+  {
+    "url": "build/agc/yye7tkov.css",
+    "revision": "b8795a407af28909068bb81efbb4fb38"
   },
   {
     "url": "index.html",
-    "revision": "e7d53963c0e43aa94054e95f459ee516"
+    "revision": "42bde3ac82e53c206b21d6b5a100593f"
   }
 ];
 
